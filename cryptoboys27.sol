86fddddd77a55c6d4dd18ad0ce4d3ba023067cb2
0xcb <36094932+aswin9393@users.noreply.github.com>
2024-02-29 12:33:31 +0800

    Add files via upload

---
+++ cryptoboys27.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
+
+import "@thirdweb-dev/contracts/base/ERC721SignatureMint.sol";
+
+contract cryptoboys27 is ERC721SignatureMint {
+    constructor(
+        address _defaultAdmin,
+        string memory _name,
+        string memory _symbol,
+        address _royaltyRecipient,
+        uint128 _royaltyBps,
+        address _primarySaleRecipient
+    )
+        ERC721SignatureMint(
+            _defaultAdmin,
+            _name,
+            _symbol,
+            _royaltyRecipient,
+            _royaltyBps,
+            _primarySaleRecipient
+        )
+    {}
+}
